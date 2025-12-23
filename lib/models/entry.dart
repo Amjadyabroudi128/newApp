91cc880cab5d76ef285cb537ddc53355b3288de3
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-12-23 20:58:07 +0000

    entry

---
+++ lib/models/entry.dart
@@ -0,0 +1,13 @@
+class Entry {
+  String name;     // SANS
+  String place;    // L.A
+  int done;        // 0
+  int total;       // 4
+
+  Entry({
+    required this.name,
+    required this.place,
+    required this.done,
+    required this.total,
+  });
+}
