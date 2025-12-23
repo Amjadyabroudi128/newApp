18d28dca5aaa450777eda6325545bf1500613b3c
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-12-23 20:58:49 +0000

    remove comments

--- lib/HomePage/HomePage.dart
+++ lib/HomePage/HomePage.dart
@@ -8,13 +8,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  // int _counter = 0;
-  //
-  // void _incrementCounter() {
-  //   setState(() {
-  //     _counter++;
-  //   });
-  // }
 
   @override
   Widget build(BuildContext context) {
@@ -33,11 +26,7 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: _incrementCounter,
-      //   tooltip: 'Increment',
-      //   child: const Icon(Icons.add),
-      // ),
+
     );
   }
 }
