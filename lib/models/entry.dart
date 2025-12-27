41c4b065a6296b0a1f0b449def5217b42bb5b66f
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2025-12-27 13:44:19 +0000

    list of entries

--- lib/models/entry.dart
+++ lib/models/entry.dart
@@ -5,3 +5,11 @@ class Entry {
 
   Entry(this.text, this.done, this.total);
 }
+final List<Entry> entries = [
+  Entry("SAINS - L.A", 0, 4),
+  Entry("C👀P - Kingsway", 0, 4),
+  Entry("B👀TS - Reigate", 2, 4),
+  Entry("C👀P - Southwater", 0, 4),
+  Entry("COUNTY MALL - Crawley", 1, 4),
+  Entry("WELCOME - Lancing", 0, 4),
+];
