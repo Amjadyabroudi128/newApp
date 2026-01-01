475db424da5905d23298cdcfe128ff40e2587ab8
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2026-01-01 17:43:33 +0000

    text field component

--- lib/HomePage/DetailsScreen.dart
+++ lib/HomePage/DetailsScreen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:intl/intl.dart';
 import 'package:to_be_named/components/kTextBtn.dart';
+import 'package:to_be_named/components/kTextField.dart';
 
 import '../models/entry.dart';
 
@@ -132,15 +133,15 @@ class _DayDetailsScreenState extends State<DayDetailsScreen> {
           content: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
-              TextField(
-                controller: doneController,
-                keyboardType: TextInputType.number,
-                decoration: const InputDecoration(labelText: "Done"),
+              MyField(
+                myController: doneController,
+                type: TextInputType.number,
+                myDecoration: const InputDecoration(labelText: "Done"),
               ),
-              TextField(
-                controller: totalController,
-                keyboardType: TextInputType.number,
-                decoration: const InputDecoration(labelText: "Total"),
+              MyField(
+                myController: totalController,
+                type: TextInputType.number,
+                myDecoration: const InputDecoration(labelText: "Total"),
               ),
             ],
           ),
