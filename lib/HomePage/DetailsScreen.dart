5f6f94b6ffbf7b5ef6417aa444e2a85b150af966
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2026-01-05 20:25:17 +0000

    save color

--- lib/HomePage/DetailsScreen.dart
+++ lib/HomePage/DetailsScreen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:intl/intl.dart';
 import 'package:to_be_named/components/kTextField.dart';
+import 'package:to_be_named/core/Colors.dart';
 
 import '../models/entry.dart';
 
@@ -106,7 +107,7 @@ class _DayDetailsScreenState extends State<DayDetailsScreen> {
             ),
             GestureDetector(
               onTap: () => Navigator.pop(context, true),
-              child: Text("Save", style: TextStyle(color: Colors.green),),
+              child: Text("Save", style: TextStyle(color: MyColors.save),),
             ),
           ],
         );
@@ -153,7 +154,7 @@ class _DayDetailsScreenState extends State<DayDetailsScreen> {
             ),
             GestureDetector(
               onTap: () => Navigator.pop(context, true),
-              child: Text("Save", style: TextStyle(color: Colors.green),),
+              child: Text("Save", style: TextStyle(color: MyColors.save),),
             ),
           ],
         );
