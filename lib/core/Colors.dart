cf927f44606e9e022d9f9afd539d9a09e4f14455
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2026-01-07 20:33:26 +0000

    toast message

--- lib/core/Colors.dart
+++ lib/core/Colors.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
 class MyColors {
   static Color save = Colors.green;
+  static Color toastMessage = Colors.white;
+  static Color toastBackGround = Colors.black54;
 }
