70ffab6a51ba138df5ae84a11d68b03e068806d9
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2026-02-03 19:44:18 +0000

    day name

--- lib/core/textStlyes.dart
+++ lib/core/textStlyes.dart
@@ -2,4 +2,8 @@ import 'package:flutter/material.dart';
 
 class MyTextStyles {
   static TextStyle empty = TextStyle(fontSize: 16, color: Colors.grey);
+  static TextStyle dayName = TextStyle(
+    fontSize: 20,
+    fontWeight: .bold,
+  );
 }
