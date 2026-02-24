b15ab906d3dc9f37662c769ff2f7ac17ebec3431
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2026-02-24 19:21:34 +0000

    fa icon

--- lib/core/icons.dart
+++ lib/core/icons.dart
@@ -1,6 +1,10 @@
 
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import 'Colors.dart';
 
 class MyIcons {
   static Icon addBtn = Icon(Icons.add);
+  static Icon cross = FaIcon(FontAwesomeIcons.crosshairs,color: MyColors.crossHair);
 }
