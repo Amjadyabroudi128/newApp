e62801aaf4eb07ba2313da3a2aaa43c0fc6360fe
Amjadyabroudi128 <amjadyabroudi.2000@gmail.com>
2026-02-27 20:41:16 +0000

    change days names

--- lib/HomePage/DetailsScreen.dart
+++ lib/HomePage/DetailsScreen.dart
@@ -31,6 +31,9 @@ class _DayDetailsScreenState extends State<DayDetailsScreen> {
       case "Tuesday":
         displayDay = "Tuesday innit";
         break;
+      case "Wednesday":
+        displayDay = "Wednesday Ma Dude";
+        break;
       case "Thursday":
         displayDay = "Thurs-whey";
         break;
